4394be31325fe166aa689ed20c64fd6c95e68666
Damian Di Giovanni <demian-dg@hotmail.com>
2024-10-20 22:04:10 -0300

    Se agregas la clase acceso a datos

---
+++ Dominio/Incidencia.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Incidencia
+    {
+        public int Id { get; set; }
+
+        public int Tipo { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public int Estado { get; set; }
+
+        public DateTime FechaAlta { get; set; }
+
+        public string FechaBaja { get; set; }
+
+        public string Resolucion { get; set; }
+
+
+    }
+}

aec4bde0dae8a43ea831ebb65508363b69b771c3
Damian Di Giovanni <demian-dg@hotmail.com>
2024-10-26 10:25:33 -0300

    Clase COmentario

---
+++ Dominio/Comentario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Comentario
+    {
+        public int id {  get; set; }
+        
+        public int Cod_Incidencia {  get; set; }
+
+        public string ComentarioGestion {  get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public int Cod_Usuario {  get; set; }
+    }
+}

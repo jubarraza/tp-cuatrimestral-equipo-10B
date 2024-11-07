ff3ad3f00d304507e206b55073a8602a5cc53e0c
Julieta <jubarraza5@gmail.com>
2024-11-07 19:26:36 -0300

    correcciones

--- App_GestorIncidencias/IncidenciaListar.aspx.designer.cs
+++ App_GestorIncidencias/IncidenciaListar.aspx.designer.cs
@@ -49,23 +49,5 @@ namespace App_GestorIncidencias
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView GridView1;
-
-        /// <summary>
-        /// Control dgvIncidencias.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView dgvIncidencias;
-
-        /// <summary>
-        /// Control btnAgregar.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnAgregar;
     }
 }

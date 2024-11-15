40ed4b7191ef1e4df4743bfe656107c82fbed326
Damian Di Giovanni <demian-dg@hotmail.com>
2024-11-15 20:27:39 -0300

    Gestion de usuario

---
+++ Negocio/GestionUser.cs
@@ -0,0 +1,44 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class GestionUser
+    {
+        public string loguear(string email, string Password)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            string legajo = "";
+            try
+            {
+                datos.setearConsulta("select Legajo from EMPLEADOS as Emp left join PERSONAS as Per on Emp.IdPersona = Per.Id where Per.Email like @User and Emp.UserPassword like @Password");
+                datos.setearParametro("@User", email);
+                datos.setearParametro("@Password", Password);
+
+                datos.ejecutarLectura();
+                while (datos.Lector.Read())
+                {
+                    legajo = datos.Lector["Legajo"].ToString();
+
+                    return legajo;
+
+                }
+                return legajo;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error de logueo", ex);
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+    }
+}

66a9922216a5a8dae20af0ba22fad901b8174a98
jonatanguzman <jonatanguzman2712@gmail.com>
2024-11-14 23:39:19 -0300

    Se Agrega Bloques TryCatch + Redirect PageError

--- App_GestorIncidencias/Admin/Empleados.aspx.cs
+++ App_GestorIncidencias/Admin/Empleados.aspx.cs
@@ -14,10 +14,21 @@ namespace App_GestorIncidencias.Admin
         public List<Empleado> listaEmpleados { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
-            EmpleadoNegocio negocio = new EmpleadoNegocio();
-            Session.Add("ListaEmpleados", negocio.listar());
-            gvEmpleados.DataSource = Session["ListaEmpleados"];             
-            gvEmpleados.DataBind();
+            try 
+            {
+                if (!IsPostBack)
+                {
+                    EmpleadoNegocio negocio = new EmpleadoNegocio();
+                    Session.Add("ListaEmpleados", negocio.listar());
+                    gvEmpleados.DataSource = Session["ListaEmpleados"];             
+                    gvEmpleados.DataBind();
+                }
+            }
+            catch (Exception ex)
+            {
+                Session.Add("Error", ex.ToString());
+                Response.Redirect("../PageError.aspx", false);
+            }
         }
 
         protected void btnAgregar_Click(object sender, EventArgs e)
@@ -27,18 +38,33 @@ namespace App_GestorIncidencias.Admin
 
         protected void gvEmpleados_SelectedIndexChanged(object sender, EventArgs e)
         {
-            string legajoSeleccionado = gvEmpleados.SelectedDataKey.Value.ToString();
-            Response.Redirect("GestionarEmpleado.aspx?Legajo=" + legajoSeleccionado);
+            try
+            {
+                string legajoSeleccionado = gvEmpleados.SelectedDataKey.Value.ToString();
+                Response.Redirect("GestionarEmpleado.aspx?Legajo=" + legajoSeleccionado);
+            }
+            catch (Exception ex)
+            {
+                Session.Add("Error", ex.ToString());
+                Response.Redirect("../PageError.aspx", false);
+            }
         }
 
         protected void txtBuscar_TextChanged(object sender, EventArgs e)
         {
-            List<Empleado> listaEmpleados = (List<Empleado>)Session["ListaEmpleados"];
-            List<Empleado> listaFiltrada = listaEmpleados.FindAll(x => x.persona.Nombre.ToUpper().Contains(txtBuscar.Text.ToUpper())
-            || x.persona.Apellido.ToUpper().Contains(txtBuscar.Text.ToUpper()));
-            gvEmpleados.DataSource = listaFiltrada;
-            gvEmpleados.DataBind();
-
+            try
+            {
+                List<Empleado> listaEmpleados = (List<Empleado>)Session["ListaEmpleados"];
+                List<Empleado> listaFiltrada = listaEmpleados.FindAll(x => x.persona.Nombre.ToUpper().Contains(txtBuscar.Text.ToUpper())
+                || x.persona.Apellido.ToUpper().Contains(txtBuscar.Text.ToUpper()));
+                gvEmpleados.DataSource = listaFiltrada;
+                gvEmpleados.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Session.Add("Error", ex.ToString());
+                Response.Redirect("../PageError.aspx", false);
+            }
         }
 
         protected void gvEmpleados_PageIndexChanging(object sender, GridViewPageEventArgs e)
--- App_GestorIncidencias/Admin/GestionarEmpleado.aspx.cs
+++ App_GestorIncidencias/Admin/GestionarEmpleado.aspx.cs
@@ -25,83 +25,98 @@ namespace App_GestorIncidencias.Admin
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            txtLegajo.Enabled = false;
+            try
+            {         
+                txtLegajo.Enabled = false;
             
-            if (!IsPostBack)
-            {                
-                EmpleadoNegocio empleadoNegocio = new EmpleadoNegocio();
-                long ultimoLegajo = empleadoNegocio.ObtenerUltimoLegajo();
-                txtLegajo.Text = ultimoLegajo.ToString();
-                TipoUsuarioNegocio tipoNegocio = new TipoUsuarioNegocio();
-                List<TipoUsuario> lista = tipoNegocio.listar();              
+                if (!IsPostBack)
+                {                
+                    EmpleadoNegocio empleadoNegocio = new EmpleadoNegocio();
+                    long ultimoLegajo = empleadoNegocio.ObtenerUltimoLegajo();
+                    txtLegajo.Text = ultimoLegajo.ToString();
+                    TipoUsuarioNegocio tipoNegocio = new TipoUsuarioNegocio();
+                    List<TipoUsuario> lista = tipoNegocio.listar();              
 
-                ddlTipoUsuario.DataSource = lista;
-                ddlTipoUsuario.DataValueField = "IdTipoUsuario";
-                ddlTipoUsuario.DataTextField = "Tipo";
-                ddlTipoUsuario.DataBind();
+                    ddlTipoUsuario.DataSource = lista;
+                    ddlTipoUsuario.DataValueField = "IdTipoUsuario";
+                    ddlTipoUsuario.DataTextField = "Tipo";
+                    ddlTipoUsuario.DataBind();
+                }
+                string legajo = Request.QueryString["Legajo"] != null ? Request.QueryString["Legajo"].ToString() : "";
+                if (legajo != "" && !IsPostBack)
+                {
+                    btnAceptar.Text = "Guardar";
+                    btnAceptar.CssClass = "btn btn-primary me-md-2";
+                    EmpleadoNegocio negocio = new EmpleadoNegocio();
+                    Empleado seleccionado = negocio.listar(legajo)[0];
+                    Session.Add("empleadoSeleccionado", seleccionado);
+                    txtNombre.Text = seleccionado.persona.Nombre;
+                    txtApellido.Text = seleccionado.persona.Apellido;
+                    txtEmail.Text = seleccionado.persona.Email;
+                    txtLegajo.Text = seleccionado.Legajo.ToString();
+                    ddlTipoUsuario.SelectedValue = seleccionado.tipoUsuario.Tipo.ToString();
+                    txtFechaIngreso.Text = seleccionado.FechaIngreso.ToString("yyyy-MM-dd");
+                    txtUserPassword.Text = seleccionado.UserPassword;
+                    bool activo = seleccionado.Activo;
+                    if(activo)
+                    {
+                        rbActivo.Checked = activo;
+                    }
+                    else
+                    {
+                        rbInactivo.Checked = !activo;
+                    }
+                    HabilitarCampos(activo);
+                }
             }
-            string legajo = Request.QueryString["Legajo"] != null ? Request.QueryString["Legajo"].ToString() : "";
-            if (legajo != "" && !IsPostBack)
+            catch (Exception ex)
             {
-                btnAceptar.Text = "Guardar";
-                btnAceptar.CssClass = "btn btn-primary me-md-2";
-                EmpleadoNegocio negocio = new EmpleadoNegocio();
-                Empleado seleccionado = negocio.listar(legajo)[0];
-                Session.Add("empleadoSeleccionado", seleccionado);
-                txtNombre.Text = seleccionado.persona.Nombre;
-                txtApellido.Text = seleccionado.persona.Apellido;
-                txtEmail.Text = seleccionado.persona.Email;
-                txtLegajo.Text = seleccionado.Legajo.ToString();
-                ddlTipoUsuario.SelectedValue = seleccionado.tipoUsuario.Tipo.ToString();
-                txtFechaIngreso.Text = seleccionado.FechaIngreso.ToString("yyyy-MM-dd");
-                txtUserPassword.Text = seleccionado.UserPassword;
-                bool activo = seleccionado.Activo;
-                if(activo)
-                {
-                    rbActivo.Checked = activo;
-                }
-                else
-                {
-                    rbInactivo.Checked = !activo;
-                }
-                HabilitarCampos(activo);
-
+                Session.Add("Error", ex.ToString());
+                Response.Redirect("../PageError.aspx", false);
             }
         }
 
         protected void btnAceptar_Click(object sender, EventArgs e)
-        {            
-            EmpleadoNegocio negocio = new EmpleadoNegocio();
-            Empleado nuevo = new Empleado();
-            nuevo.persona = new Persona();
-            nuevo.persona.Nombre = txtNombre.Text;
-            nuevo.persona.Apellido = txtApellido.Text;
-            nuevo.persona.Email = txtEmail.Text;
-            nuevo.tipoUsuario = new TipoUsuario();
-            nuevo.tipoUsuario.IdTipoUsuario = int.Parse(ddlTipoUsuario.SelectedValue);
-            nuevo.FechaIngreso = DateTime.Parse(txtFechaIngreso.Text);
-            nuevo.UserPassword = txtUserPassword.Text;
-            if (rbActivo.Checked || rbInactivo.Checked)
-            {
-                nuevo.Activo = rbActivo.Checked;
-                lblError.Visible = false;
+        {
+            try 
+            { 
+                EmpleadoNegocio negocio = new EmpleadoNegocio();
+                Empleado nuevo = new Empleado();
+                nuevo.persona = new Persona();
+                nuevo.persona.Nombre = txtNombre.Text;
+                nuevo.persona.Apellido = txtApellido.Text;
+                nuevo.persona.Email = txtEmail.Text;
+                nuevo.tipoUsuario = new TipoUsuario();
+                nuevo.tipoUsuario.IdTipoUsuario = int.Parse(ddlTipoUsuario.SelectedValue);
+                nuevo.FechaIngreso = DateTime.Parse(txtFechaIngreso.Text);
+                nuevo.UserPassword = txtUserPassword.Text;
+                if (rbActivo.Checked || rbInactivo.Checked)
+                {
+                    nuevo.Activo = rbActivo.Checked;
+                    lblError.Visible = false;
+                }
+                else
+                {
+                    lblError.Visible = true;
+                    return;
+                }
+
+                if (Request.QueryString["Legajo"] != null)
+                {
+                    nuevo.Legajo = int.Parse(Request.QueryString["Legajo"]);
+                    negocio.Modificar(nuevo);
+                }
+                else
+                {
+                    negocio.Agregar(nuevo);
+                }           
             }
-            else
+            catch (Exception ex)
             {
-                lblError.Visible = true;
-                return;
+                Session.Add("Error", ex.ToString());
+                Response.Redirect("../PageError.aspx", false);
             }
 
-            if (Request.QueryString["Legajo"] != null)
-            {
-                nuevo.Legajo = int.Parse(Request.QueryString["Legajo"]);
-                negocio.Modificar(nuevo);
-            }
-            else
-            {
-                negocio.Agregar(nuevo);
-            }
-            
             Response.Redirect("~/Admin/Empleados.aspx");
 
         }
@@ -121,16 +136,18 @@ namespace App_GestorIncidencias.Admin
             }
             catch (Exception ex)
             {
-                throw ex;
+                Session.Add("Error", ex.ToString());
+                Response.Redirect("../PageError.aspx", false);
             }
+
             Response.Redirect("~/Admin/Empleados.aspx");
+
         }
 
         protected void rbActivo_CheckedChanged(object sender, EventArgs e)
         {
             bool habilitar = rbActivo.Checked;
-            HabilitarCampos(habilitar);         
-         
+            HabilitarCampos(habilitar);       
         }
 
     }

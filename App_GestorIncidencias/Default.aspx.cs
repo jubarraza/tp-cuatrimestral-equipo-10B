3933b39097efe8ae1cdf85bec6317c8c7ac02888
Damian Di Giovanni <demian-dg@hotmail.com>
2024-11-15 22:55:32 -0300

    Funcionalidad al inicio de session

--- App_GestorIncidencias/Default.aspx.cs
+++ App_GestorIncidencias/Default.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using App_GestorIncidencias.Admin;
+using Dominio;
+using Negocio;
 
 namespace App_GestorIncidencias
 {
@@ -16,7 +19,34 @@ namespace App_GestorIncidencias
 
         protected void btnIngresar_Click(object sender, EventArgs e)
         {
+            AccesoDatos acceso = new AccesoDatos();
+            EmpleadoNegocio empleadoNegocio = new EmpleadoNegocio();
+            try
+            {
+                GestionUser gestionUser = new GestionUser();
+                string leg = gestionUser.loguear(txtEmail.Text, txtPass.Text);
+                if (leg != "")
+                {
+                    Empleado empleadouser = (empleadoNegocio.listar(leg)[0]);
+                    Session.Add("usuario", empleadouser);
+                    //Response.Redirect("", false);
+                }
+                else
+                {
+                    Session.Add("error", "user o pass incorrectos");
+                    Response.Redirect("PageError.aspx", false);
+                }
 
+            }
+            catch (Exception ex)
+            {
+                Session.Add("error", ex.ToString());
+                Response.Redirect("PageError.aspx");
+            }
+            finally
+            {
+                acceso.cerrarConexion();
+            }
         }
     }
 }
